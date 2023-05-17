dbf85612eb97612731182144049575346a7249e3
Manish Chitre <manish_chitre@hotmail.com>
2023-05-17 23:32:39 +0530

    preventing duplicate reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -29,6 +29,7 @@ const mongoose = require('mongoose');const tourSchema = mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be less than 5.0'],
+      set: (val) => Math.round(val * 10) / 10, //4.666666 , 46.666, 47, 4.7
     },
     ratingsQuantity: {
       type: Number,
